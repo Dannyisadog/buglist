98a4967020539f5cba7ce03107532856bbf4be48
Dannyisadog <dannyisadog10@gmail.com>
2021-05-01 15:27:32 +0800

    feat: remove test files

--- resources/js/components/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Dashboard = () => {
-    return (
-        <h3>Welcome to the Dashboard page.</h3>
-    );
-}
-
-export default Dashboard;
--- resources/js/components/ItemList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ItemList = () => {
-    return (
-        <h3>Welcome to the Item List page.</h3>
-    );
-}
-
-export default ItemList;
